89cb8d627e4c48d27deec3033431c51eb9d31fb7
AltamiroF-22 <altamiroribeirodarocha@gmail.com>
2024-01-30 15:31:23 -0300

    ...

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -5,9 +5,10 @@ import { useEffect, useState } from "react";
 //components
 import Header from "./Header/Header";
 import About from "./about/About";
-import { SelectedProjects } from "./selectedProjects/selectedProjects";
-
 import MagnetoCircle from "../../components/magnetoCirlcle/MagnetoCorcle";
+import { SelectedProjectsData } from "../../data/projects/SelectedProjects";
+import { ProjectComponent} from "../../components/projectsSelected/ProjectsSelected";
+import TextEffect from "../../components/textEffect/TextEffect";
 
 //lenis scroll smooth
 import Lenis from "@studio-freight/lenis";
@@ -18,6 +19,7 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 //styles
 import "./Home.sass";
+import './SelectedProjects.sass'
 
 //images
 import ArrowsUP from "../../assets/arrow/ArrowsUP.svg";
@@ -52,7 +54,28 @@ const Home = () => {
     <div className="home-page" id="home">
       <Header />
       <About />
-      <SelectedProjects />
+      <main className="selectedProjects">
+      <div className="selected-rojects-title">
+        <TextEffect
+          text1="selected projects"
+          fontSize="Size30"
+          fontWeigth="W700"
+        />
+      </div>
+
+      <section className="project">
+        {SelectedProjectsData.map((item) => (
+          <div key={item.id}>
+            <ProjectComponent
+              h2={item.name}
+              src={item.image}
+              alt={item.name}
+              href={item.project_live}
+            />
+          </div>
+        ))}
+      </section>
+    </main>
       <a href="#home" className="back-to-top">
         {showBackUpArrow ? (
           <MagnetoCircle

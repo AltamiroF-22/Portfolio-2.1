ccb1269c170af0cc647d91f7ed1896094f4884a0
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-04-08 03:36:56 -0300

    new project added

--- src/data/projects/SelectedProjects.jsx
+++ src/data/projects/SelectedProjects.jsx
@@ -1,16 +1,17 @@
 import IPAddressImage from "../../assets/imgsWebpack/IP-Address.webp";
 import SpaceImage from "../../assets/imgsWebpack/Space.webp";
-import PedraPapelImage from "../../assets/imgsWebpack/pedra-papel.webp";  
-import RestCountryImage from "../../assets/imgsWebpack/rest-country.webp";  
-import DpFlexImage from "../../assets/imgsWebpack/dp-flex.webp";  
+import PedraPapelImage from "../../assets/imgsWebpack/pedra-papel.webp";
+import RestCountryImage from "../../assets/imgsWebpack/rest-country.webp";
+import DpFlexImage from "../../assets/imgsWebpack/dp-flex.webp";
+import ToDoList from "../../assets/imgs/MERN_TO_DO_LIST.webp";
 
 export const SelectedProjectsData = [
   {
-    id: 1,
-    name: "REST Countries API...",
-    alt: "REST Countries API screenshot",
-    image: RestCountryImage,
-    project_live: "https://snazzy-malasada-b4cc16.netlify.app/"
+    id: 6,
+    name: "MERN TO-DO List (Login Register+)",
+    alt: "MERN TO-DO List (Login Register+)screenshot",
+    image: ToDoList,
+    project_live: "https://fullstack-mern-todo-list.netlify.app/",
   },
   {
     id: 5,
@@ -18,28 +19,34 @@ export const SelectedProjectsData = [
     alt: "Display-Flex Config screenshot",
     image: DpFlexImage,
     project_live:
-      "https://64efd8178cf9c304f36e4ead--deluxe-profiterole-fea8eb.netlify.app/"
+      "https://64efd8178cf9c304f36e4ead--deluxe-profiterole-fea8eb.netlify.app/",
+  },
+  {
+    id: 1,
+    name: "REST Countries API...",
+    alt: "REST Countries API screenshot",
+    image: RestCountryImage,
+    project_live: "https://snazzy-malasada-b4cc16.netlify.app/",
   },
   {
     id: 2,
     name: "IP Address Tracker",
     alt: "IP Address Tracker screenshot",
     image: IPAddressImage,
-    project_live: "https://elegant-fudge-d486ff.netlify.app/"
+    project_live: "https://elegant-fudge-d486ff.netlify.app/",
   },
   {
     id: 3,
     name: "Space Tourism",
     alt: "Space Tourism screenshot",
     image: SpaceImage,
-    project_live: "https://cerulean-gingersnap-90ed6f.netlify.app/"
+    project_live: "https://cerulean-gingersnap-90ed6f.netlify.app/",
   },
   {
     id: 4,
     name: "Rock Paper Scissors",
     alt: "Rock Paper Scissors screenshot",
     image: PedraPapelImage,
-    project_live: "https://magical-duckanoo-d9dbfc.netlify.app/"
+    project_live: "https://magical-duckanoo-d9dbfc.netlify.app/",
   },
-  
 ];
